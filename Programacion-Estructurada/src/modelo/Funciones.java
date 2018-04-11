0034a78f73f94f0d7507ed7bd99b6373b43ac926
MauroLucas <m_l_pereyra@hotmail.com>
2018-04-11 17:13:24 -0300

    Adjunto ejemplo de javadoc

---
+++ Programacion-Estructurada/src/modelo/Funciones.java
@@ -0,0 +1,20 @@
+package modelo;
+/**
+ * Esta Clase define un metodo basico de logica
+ * matematica,sumar 2 numeros
+ * @author Mauro
+ *
+ */
+public class Funciones {
+	/**
+	 * metodo que permite sumar dos sumeros pasados
+	 * por parametro
+	 * @param num1 - Valor ingresado por parametro
+	 * @param num2 - Valor ingresado por parametro
+	 * @return - Retorna la suma de num1 y num2
+	 */
+	public static int sumar(int num1,int num2) {
+		return num1+num2;
+	}
+
+}
---
+++ Programacion-Estructurada/src/test/TestFunciones.java
@@ -0,0 +1,22 @@
+package test;
+import modelo.Funciones;
+/**
+ * Clase para el testeo de la clase main
+ * @author Mauro
+ *
+ */
+public class TestFunciones {
+
+	/**
+	 * Metodo princial
+	 * @param args - Array de String
+	 */
+	public static void main(String[] args) {
+		
+		int num1 = 3;
+		int num2 = 2;
+		System.out.print(Funciones.sumar(num1, num2));
+
+	}
+
+}
