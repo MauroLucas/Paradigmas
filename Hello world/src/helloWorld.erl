b8e078bae729d5d5b633c3e3b3e94f0f1f54c22d
MauroLucas <m_l_pereyra@hotmail.com>
2018-06-18 18:32:11 -0300

    Delete

--- Hello world/src/helloWorld.erl
+++
@@ -1,6 +0,0 @@
-% hello world program
--module(helloWorld). 
--export([start/0]). 
-
-start() -> 
-   io:fwrite("Hello, world!\n").
--- Herencia/src/modelo/ICocinero.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public interface ICocinero {
-	
-	String cocinar();
-
-}
